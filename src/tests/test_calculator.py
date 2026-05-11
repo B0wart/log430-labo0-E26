f5b4849062a15f150e179b4968ed5f3c80239cdd
Rayan Laihem <rylaihem@gmail.com>
2026-05-11 14:54:50 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -3,7 +3,7 @@ Calculator app tests
 SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
-
+import pytest
 from calculator import Calculator
 
 def test_app():
@@ -11,4 +11,23 @@ def test_app():
     welcome_message = my_calculator.get_hello_message()
     assert "== Calculatrice v1.0 ==" in welcome_message
 
-# TODO: ajoutez les tests
+def test_addition():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
+
+def test_substract():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(3, 2) == 1
+
+def test_multiply():
+    my_calculator = Calculator()
+    assert my_calculator.multiplication(3, 2) == 6
+
+def test_division():
+    my_calculator = Calculator()
+    assert my_calculator.division(4, 2) == 2
+
+def test_addition_failure():
+    my_calculator = Calculator()
+    assert my_calculator.addition(4, 0) == 0
+    
